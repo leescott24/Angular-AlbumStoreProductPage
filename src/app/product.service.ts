b51b7c49755644269ebfd4e73526935e162b0184
Lee <lee.scott@neueda.com>
2018-09-04 15:45:14 +0100

    mod 4.4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,15 +5,21 @@ import { Http } from '@angular/http'
 import 'rxjs/add/operator/map'
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((r) => <Album>r.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productUrl).map((r) => <Product[]>r.json())
   }
 }
